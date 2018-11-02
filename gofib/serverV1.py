016aa7c1a8b4a033b39dd2c2c12aa76b2f4905a1
lexkwan <lexkwan@163.com>
2018-11-02 08:01:19 +0800

    add python codes

---
+++ gofib/serverV1.py
@@ -0,0 +1,31 @@
+from socket import *
+
+def fib(n):
+    if n <= 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def fib_server(address):
+    sock = socket(AF_INET,SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+    sock.bind(address)
+    sock.listen(5)
+    while True:
+        client, addr = sock.accept()
+        print("Connection",addr)
+        fib_handler(client)
+
+def fib_handler(client):
+    while True:
+        req = client.recv(100)
+        if not req:
+            break
+        n = int(req)
+        result = fib(n)
+        resp = str(result).encode('ascii') +b'\n'
+        client.send(resp)
+    print("Closed")
+
+fib_server(('',25000))
